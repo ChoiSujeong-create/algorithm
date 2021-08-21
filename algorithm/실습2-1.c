6741f4ef9a3512cdd3cd8e2f32224711f188eb7b
ChoiSujeong-create <67599179+ChoiSujeong-create@users.noreply.github.com>
2021-08-21 23:58:14 +0900

    21-08-21

---
+++ algorithm/실습2-1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define N 5
+int main(void) {
+	int i;
+	int a[N]; /*배열의 선언*/
+	for (int i = 0; i < N; i++) {
+		printf("a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+	puts("각 요소의 값");
+	for (i = 0; i < N; i++) {
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+	return 0;
+}
